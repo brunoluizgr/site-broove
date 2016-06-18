b99f977a0f3618e84fe6ce91c60115ac1764e65a
brunoluizgr <brunoluizgr@gmail.com>
2016-06-17 22:34:18 -0300

    Major fixes do site. Adição da página 404 Not Found e Página de Apresentação do EP fixada.

--- wp-content/themes/tema-de-broove/404.php
+++ wp-content/themes/tema-de-broove/404.php
@@ -1,15 +1,16 @@
-<?php get_header(); ?>
+<?php get_header('interno'); ?>
 
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-					</main>
-
+			<section id="pagina-nao-encontrada" class="container-fluid padding-t-5 margin-t-4 margin-b-2">
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 text-center">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<h1><?php _e('Pagina nao encontrada!', 'temadebroove') ?></h1>
+							<p><i id="nota-nao-encontrada" class="fa fa-music fa-4x"></i><p>
+								<p><?php _e('Parece que você tocou uma nota errada e acessou uma página que não está disponível ou foi removida. =X', 'temadebroove') ?></p>
+								<p class="margin-t-2"><a href="<?php echo(get_site_url()) ?>"><button id="btn-volta-home" class="btn"><?php _e('Voltar ao Inicio', 'temadebroove') ?></button></a></p>
+						</div>
+					</div>
 				</div>
-
-			</div>
+			</section>
 
 <?php get_footer(); ?>
